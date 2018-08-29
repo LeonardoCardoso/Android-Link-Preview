213d4a3acc02aae84f38650b5406d9063c5bc7e7
jdonmoyer <jasondonmoyer@gmail.com>
2018-08-29 10:57:21 -0400

    Refactor code into BaseImagePickingStrategy so that it can be reused by subclasses

--- library/src/main/java/com/leocardz/link/preview/library/BaseImagePickingStrategy.java
+++ library/src/main/java/com/leocardz/link/preview/library/BaseImagePickingStrategy.java
@@ -1,5 +1,15 @@
 package com.leocardz.link.preview.library;
 
+import android.os.AsyncTask;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public abstract class BaseImagePickingStrategy implements ImagePickingStrategy {
     private int imageQuantity = TextCrawler.ALL;
 
@@ -11,5 +21,33 @@ public abstract class BaseImagePickingStrategy implements ImagePickingStrategy {
     @Override
     public int getImageQuantity() {
         return imageQuantity;
+    }
+
+    protected List<String> getMetaImage(HashMap<String, String> metaTags) {
+        List<String> images = new ArrayList<>();
+        final String metaImage = metaTags.get("image");
+
+        if (!metaImage.equals("")) {
+            images.add(metaImage);
+        }
+        return images;
+    }
+
+    protected List<String> getImagesFromImgTags(AsyncTask asyncTask, Document document) {
+        List<String> images = new ArrayList<>();
+        Elements media = document.select("[src]");
+
+        for (Element srcElement : media) {
+            if (asyncTask.isCancelled()) {
+                break;
+            }
+            if (srcElement.tagName().equals("img")) {
+                images.add(srcElement.attr("abs:src"));
+                if (getImageQuantity() != TextCrawler.ALL && images.size() == getImageQuantity()) {
+                    break;
+                }
+            }
+        }
+        return images;
     }
 }
